2ab35390d42893165e4380d51c08ff537d34c87c
shulaev ivan <mail@ntdservice.ru>
2024-12-02 23:15:58 +0300

    add some fixes

--- src/components/ForClients/ForClients.jsx
+++ src/components/ForClients/ForClients.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import InnerPageHeader from "../InnerPageHeader/InnerPageHeader";
+import { forClientsFaqShowDescription } from "../../redux/slices/innerPageSlice";
 import supplerDemo from '../../img/supplersImages/suppler_demo.png';
 import proteiLogo from '../../img/supplersImages/protei.svg';
 import calculateLogo from '../../img/supplersImages/calculate.svg';
@@ -9,9 +10,20 @@ import upakovkaLogo from '../../img/supplersImages/upakovka24.svg';
 import plasticKitLogo from '../../img/supplersImages/plasticKit.svg';
 import vitaplastLogo from '../../img/supplersImages/vitaplast.svg';
 import cosmopackLogo from '../../img/supplersImages/cosmopack.svg';
+import okilsatoLogo from '../../img/supplersImages/okilsato.svg';
+import stickylineLogo from '../../img/supplersImages/stickyline.png';
+import mdmLogo from '../../img/supplersImages/mdm.png';
+import rpkLogo from '../../img/supplersImages/rpkr.png';
+import alaskaLogo from '../../img/supplersImages/alaska.jpg';
+import gospodinLogo from '../../img/supplersImages/pechatnik.png';
 
 const ForClients = () => {
     const forClientsState = useSelector((state) => state.innerPage.forClientsPage);
+    const dispatch = useDispatch();
+
+    const faqDescriptionHandler = (faqItem) => {
+        dispatch(forClientsFaqShowDescription({faqId: faqItem.id}))
+    };
 
     return (
         <React.Fragment>
@@ -148,10 +160,108 @@ const ForClients = () => {
                                 <div className="for-clients-supplier-main-title">
                                     <h3>Дизайн</h3>
                                 </div>
+                                <div className="clients-suppliers-wrap">
+                                    <div className="clients-supplier-item-wrap">
+                                        <div className="clients-supplier-item-head">
+                                            <div className="clients-supplier-item-head-city">Город</div>
+                                            <div className="clients-supplier-item-head-phone">Телефон</div>
+                                            <div className="clients-supplier-item-head-logo"></div>
+                                        </div>
+                                        <div className="clients-supplier-item-row">
+                                            <div className="clients-supplier-item-description">
+                                                Санкт-Петербург
+                                                <Link to={'+78123275737'}>+7 (812) 327-57-37</Link>
+                                            </div>
+                                            <div className="clients-supplier-item-logo">
+                                                <Link 
+                                                    to={'https://okil.ru/en/contact.html'}
+                                                    target={'_blank'}
+                                                >
+                                                    <img src={okilsatoLogo} alt="#" />
+                                                </Link>
+                                            </div>
+                                        </div>
+                                        <div className="clients-supplier-item-row">
+                                            <div className="clients-supplier-item-description">
+                                                Санкт-Петербург
+                                                <Link to={'+78123356630'}>+7 (812) 335-66-30</Link>
+                                            </div>
+                                            <div className="clients-supplier-item-logo">
+                                                <Link 
+                                                    to={'https://stickyline.ru/contacts/'}
+                                                    target={'_blank'}
+                                                >
+                                                    <img src={stickylineLogo} alt="#" />
+                                                </Link>
+                                            </div>
+                                        </div>
+
+                                        <div className="clients-supplier-item-row">
+                                            <div className="clients-supplier-item-description">
+                                                Москва
+                                                <Link to={'+74952561000'}>+7 (495) 256-10-00</Link>
+                                            </div>
+                                            <div className="clients-supplier-item-logo">
+                                                <Link 
+                                                    to={'https://mdmprint.ru/contacts/'}
+                                                    target={'_blank'}
+                                                >
+                                                    <img src={mdmLogo} alt="#" />
+                                                </Link>
+                                            </div>
+                                        </div>
+
+                                        <div className="clients-supplier-item-row">
+                                            <div className="clients-supplier-item-description">
+                                                Санкт-Петербург
+                                                <Link to={'+78129859448'}>+7 (812) 985-94-48</Link>
+                                            </div>
+                                            <div className="clients-supplier-item-logo">
+                                                <Link 
+                                                    to={'https://rpkgr.ru/kontakty/'}
+                                                    target={'_blank'}
+                                                >
+                                                    <img src={rpkLogo} alt="#" />
+                                                </Link>
+                                            </div>
+                                        </div>
+
+                                        <div className="clients-supplier-item-row">
+                                            <div className="clients-supplier-item-description">
+                                                Санкт-Петербург
+                                                <Link to={'+78123803359'}>+7 (812) 380-33-59</Link>
+                                            </div>
+                                            <div className="clients-supplier-item-logo">
+                                                <Link 
+                                                    to={'https://www.alaskapof.ru/contacts/'}
+                                                    target={'_blank'}
+                                                >
+                                                    <img src={alaskaLogo} alt="#" />
+                                                </Link>
+                                            </div>
+                                        </div>
+
+                                        <div className="clients-supplier-item-row">
+                                            <div className="clients-supplier-item-description">
+                                                Санкт-Петербург
+                                                <Link to={'+78127708090'}>+7 (812) 770-80-90</Link>
+                                            </div>
+                                            <div className="clients-supplier-item-logo">
+                                                <Link 
+                                                    to={'https://gospechatnik.ru/'}
+                                                    target={'_blank'}
+                                                >
+                                                    <img src={gospodinLogo} alt="#" />
+                                                </Link>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </section>
+
                 <section>
                     <div className="container">
                         <h2>Порядок работы</h2>
@@ -162,7 +272,7 @@ const ForClients = () => {
                                     <React.Fragment key={stepItem.id}>
                                         <li>
                                             <p className="event-date">{stepItem.name}</p>
-                                            <p className="event-description">HTML Tags, an informal CERN document listing 18 HTML tags, was first mentioned in public.</p>
+                                            <p className="event-description">{stepItem.description}</p>
                                         </li>
                                     </React.Fragment>
                                 )
@@ -174,12 +284,32 @@ const ForClients = () => {
                 </section>
                 <section>
                     <div className="container">
-                        <h2>Вопросы по услугам и доставке</h2>
-                    </div>
-                </section>
-                <section>
-                    <div className="container">
-                        <h2>Шаблоны</h2>
+                        <h2>Часто задаваемые вопросы</h2>
+                        <div className="forclients-faq-main-row">
+                            {forClientsState.faqQuestions.map((faqItem) => {
+                                return (
+                                    <React.Fragment key={faqItem.id}>
+                                        <div className={faqItem.active ? "for-clients-question-item-row active" : "for-clients-question-item-row"}>
+                                            <div className="for-clients-question-title-wrap"><h4>{faqItem.ask}</h4></div>
+                                            <div className="for-clients-question-btn-wrap">
+                                                <span 
+                                                    className={faqItem.active ? "for-clients-question-btn-more rotate-for-clients-question-btn-more" 
+                                                        : "question-item-btn-more"
+                                                    }
+                                                    onClick={() => faqDescriptionHandler(faqItem)}
+                                                ></span>
+                                            </div>
+                                            {faqItem.active ? 
+                                                <div className={"for-clients-question-item-description"}>
+                                                    asdasdasd
+                                                </div>
+                                            
+                                            : null}
+                                        </div>
+                                    </React.Fragment>
+                                )
+                            })}
+                        </div>
                     </div>
                 </section>
             </div>
