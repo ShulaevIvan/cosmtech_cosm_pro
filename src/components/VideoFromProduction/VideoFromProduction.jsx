02d93951b9b2d6282bfdd5068b6814e2cd4d9c91
shulaev ivan <mail@ntdservice.ru>
2025-04-27 21:51:09 +0300

    add some fixes

---
+++ src/components/VideoFromProduction/VideoFromProduction.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const VideoFromProduction = () => {
+    return (
+        <React.Fragment>
+            asdasd
+        </React.Fragment>
+    )
+};
+
+export default VideoFromProduction;
